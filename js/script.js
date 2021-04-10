49c7419fd6ac4613ebac4be1c7cc2f781659c16e
mreadv <mariareadv@gmail.com>
2021-04-10 11:58:43 -0400

    Added semicolons

--- js/script.js
+++ js/script.js
@@ -7,6 +7,6 @@ const getImage = async function () {
     const res = await fetch ("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
     console.log(images);
-}
+};
 
-getImage()
+getImage();
